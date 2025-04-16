5388b38634eb96b4ffbdfdf5d1470a34aeb481cb
nelsonwm45 <nelsonwm45@gmail.com>
2025-04-16 21:36:33 +0800

    Added some test cases

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -13,8 +13,10 @@ int	main(void)
 	ct1.printStatus();
 	ct4.printStatus();
 	ct5.printStatus();
-	ct1.setAttackDmg(5);
-	ct1.attack("Banana");
+
+
+	ct1.setAttackDmg(5); // set the attack dmg of ct1 to 5
+	ct1.attack("Banana"); // ct1 attack all banana claptrap
 	ct4.takeDamage(ct1.getAttackDmg());
 	ct5.takeDamage(ct1.getAttackDmg());
 	ct4.takeDamage(ct1.getAttackDmg());
@@ -23,9 +25,14 @@ int	main(void)
 	ct1.printStatus();
 	ct4.printStatus();
 	ct5.printStatus();
-	ct1.beRepaired(2);
-	ct4.beRepaired(2);
-	ct5.beRepaired(2);
+	ct1.beRepaired(2); // can repair
+	ct4.beRepaired(2); // should be cannot repair
+	ct5.beRepaired(2); // cannot repair
+	ct4.attack("Banana"); // cannot attack
+	ct5.attack("Banana"); // cannot attack
+	ct1.attack("Banana"); // can attack
+	ct4.takeDamage(ct1.getAttackDmg()); // suppose ded and cannot take dmg
+	ct5.takeDamage(ct1.getAttackDmg()); // suppose ded
 	ct1.printStatus();
 	ct4.printStatus();
 	ct5.printStatus();
